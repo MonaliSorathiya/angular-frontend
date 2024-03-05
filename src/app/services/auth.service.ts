a2402721a43340186618902646dad65fe4e241bd
monalisorathiya <monalisorathiya8@gmail.com>
2024-03-06 01:43:52 +0530

    Added angular build

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,20 +6,20 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class AuthService {
-  // baseURL: string = 'http://localhost:3000'; // For Local
-  baseURL: string = 'http://ec2-3-108-234-199.ap-south-1.compute.amazonaws.com' // For production
+  // baseURL: string = 'http://localhost:3000/'; // For Local
+  baseURL: string = 'http://ec2-3-108-234-199.ap-south-1.compute.amazonaws.com/' // For production
 
   constructor(
     private http: HttpClient,
   ) { }
 
   registerNewUser(user: any) {
-    let url = `${this.baseURL}/api/userDetails/registerNewCustomer`;
+    let url = `${this.baseURL}api/registerNewCustomer`;
     console.log('url: registerURL', url);
     return this.http.post<any>(url, user);
   }
   login(auth: any) {
-    let url = `${this.baseURL}/api/userDetails/loginCustomer`;
+    let url = `${this.baseURL}api/loginCustomer`;
     console.log('url:loginURL ', url);
     return this.http.post<any>(url, auth);
   }
